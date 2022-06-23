02e27cf63e79531bf6ac90c5e70aa561fe7f63eb
herhighnesy <105574974+herhighnesy@users.noreply.github.com>
2022-06-23 22:22:39 +0200

    Initial commit

---
+++ calc.js
@@ -0,0 +1,37 @@
+function Calculator() {
+    let newLine = "\r\n"
+    let msg = "Your Input is Incorrect."
+    msg += newLine;
+    msg += "Press F5 and this time around,"
+    msg += newLine;
+    msg += "Read the ******* instructions."
+
+
+
+    let name = prompt("What is your name?");
+    console.log(`Okay ${name}`);
+    let first_input = parseInt(prompt("What is your first number?"));
+    let second_input = parseInt(prompt("What is your second number?"));
+    let operator = prompt("What is your operator?")
+
+    if (operator === "+") {
+        let addition = first_input+second_input;
+        alert(`${name} Your answer is ${addition}`)
+    } else if (operator === "-") {
+        let subtraction = first_input-second_input;
+        alert(`${name} Your answer is ${subtraction}`)
+    } else if (operator === "/") {
+        let division = first_input/second_input;
+        alert(`${name} Your answer is ${division}`)
+    } else if (operator === "%") {
+        let remainder = first_input%second_input;
+        alert(`${name} Your answer is ${remainder}`)
+    } else if (operator === "*") {
+        let multiplication = first_input*second_input;
+        alert(`${name} Your answer is ${multiplication}`)
+    } else {
+        alert(msg);
+    }
+}
+
+Calculator()
